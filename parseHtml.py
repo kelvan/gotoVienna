7506abe9c7a76aacd956ddf78ca0361f85db1aca
Florian Schweikert <kelvan@logic.at>
2011-03-03 18:13:50 +0100

    added ParserError class and parser states raising Error when parsing incorrect data added docstring for details

--- parseHtml.py
+++ parseHtml.py
@@ -4,10 +4,22 @@ from datetime import time, datetime
 from textwrap import wrap
 import settings
 
+class ParserError(Exception):
+     def __init__(self, value='', code=0):
+         self.value = value
+         self.code = code
+
+     def __str__(self):
+         return repr(self.value)
 
 class Parser:
     _overview = None
     _details = None
+    STATE_ERROR = -1
+    STATE_START = 0
+    STATE_SEARCH = 1
+    STATE_RESULT = 2
+    _current_state = 0
 
     def __init__(self, html):
         self.soup = BeautifulSoup(html)
@@ -18,6 +30,9 @@ class Parser:
         raise IndexError()
 
     def _parse_details(self):
+        if self._current_state < 0:
+            raise ParserError('Unable to parse details while in error state')
+
         trips = map(lambda x: map(lambda x: {
                                              # TODO kick out wrap
                         'time': map(lambda x: (time(*map(lambda x: int(x), x.split(':')))), wrap(x.find('td', {'class': 'col_time'}).text, 5)), # black magic appears
@@ -31,45 +46,65 @@ class Parser:
 
     @property
     def details(self):
+        """returns list of trip details
+        [ [ { 'time': [datetime.time, datetime.time] if time else [],
+              'station': [u'start', u'end'] if station else [],
+              'info': [u'start station' if station else u'details for walking', u'end station' if station else u'walking duration']
+            }, ... # next trip step 
+          ], ... # next trip possibility
+        ]
+        """
         if not self._details:
             self._details = self._parse_details()
 
         return self._details
 
     def _parse_overview(self):
-        """
-        Returns dict containing
+        # get overview table
+        table = self.soup.find('table', {'id': 'tbl_fahrten'})
+
+        # check if there is an overview table
+        if table and table.findAll('tr'):
+            # get rows
+            rows = table.findAll('tr')[1:] # cut off headline
+            overview = map(lambda x: {
+                               'date': datetime.strptime(x.find('td', {'class': 'col_date'}).text, '%d.%m.%Y') # grab date
+                                           if x.find('td', {'class': 'col_date'}).text else None, # if date is empty set to None
+                               'time': map(lambda x: time(*map(lambda x: int(x), x.split(':'))) if x else None, # extract times or set to None if empty
+                                           x.find('td', {'class': 'col_time'}).text.split('  - ')) if x.find('td', {'class': 'col_time'}) else [],
+                               'duration': time(*map(lambda x: int(x), x.find('td', {'class': 'col_duration'}).text.split(':'))), # grab duration
+                               'change': int(x.find('td', {'class': 'col_change'}).text) # grab changes
+                                           if x.find('td', {'class': 'col_change'}).text else 0, # if change is empty set to 0
+                               'price': float(x.find('td', {'class': 'col_price'}).text.replace(',', '.')) # grab price
+                                           if x.find('td', {'class': 'col_price'}).text.find(',') >= 0 else 0.0, # if price is empty set to 0.0
+                           },
+                           rows)
+        else:
+            self._current_state = self.STATE_ERROR
+            raise ParserError('Unable to parse details while in error state')
+
+        return overview
+
+    @property
+    def overview(self):
+        """dict containing
         date: datetime
         time: [time, time]
         duration: time
         change: int
         price: float
         """
-        # get overview table
-        table = self.soup.find('table', {'id': 'tbl_fahrten'})
-        # get rows
-        rows = table.findAll('tr')[1:]
-        overview = map(lambda x: {
-                           'date': datetime.strptime(x.find('td', {'class': 'col_date'}).text, '%d.%m.%Y') # grab date
-                                       if x.find('td', {'class': 'col_date'}).text else None, # if date is empty set to None
-                           'time': map(lambda x: time(*map(lambda x: int(x), x.split(':'))) if x else None, # extract times or set to None if empty
-                                       x.find('td', {'class': 'col_time'}).text.split('  - ')),
-                           'duration': time(*map(lambda x: int(x), x.find('td', {'class': 'col_duration'}).text.split(':'))), # grab duration
-                           'change': int(x.find('td', {'class': 'col_change'}).text) # grab changes
-                                       if x.find('td', {'class': 'col_change'}).text else 0, # if change is empty set to 0
-                           'price': float(x.find('td', {'class': 'col_price'}).text.replace(',', '.')) # grab price
-                                       if x.find('td', {'class': 'col_price'}).text.find(',') >= 0 else 0.0, # if price is empty set to 0.0
-                       },
-                       rows)
-
-        return overview
-
-    @property
-    def overview(self):
         if not self._overview:
             self._overview = self._parse_overview()
 
         return self._overview
+
+    def _check_request_state(self):
+        raise NotImplementedError()
+
+    @property
+    def request_state(self):
+        return self._current_state
 
 
 class iTipParser:
